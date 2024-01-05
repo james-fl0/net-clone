78d83ba350b3f05157508aee4c43ff69841dc5ab
Matias Gonzalez <matiasnahuelgonzalez01@gmail.com>
2024-01-05 14:25:21 -0300

    verce.json update

--- server/src/index.js
+++ server/src/index.js
@@ -9,16 +9,21 @@ import { config as configDotenv } from "dotenv";
 configDotenv()
 
 const app = express()
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+// app.use(cors({
+//   origin: 'http://localhost:3000', 
+//   credentials: true,  
+// }))
 app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true,  
-}))
+  origin: 'http://localhost:3000', // o el dominio de tu cliente en desarrollo
+  methods: ['GET', 'OPTIONS', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
 
 app.use(express.json())
 app.use(sessions)
