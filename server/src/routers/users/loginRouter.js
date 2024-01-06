e831a810b572c617e21f23c66456b58c820d4480
Matias Gonzalez <matiasnahuelgonzalez01@gmail.com>
2024-01-06 17:19:43 -0300

    cookie modify 37

--- server/src/routers/users/loginRouter.js
+++ server/src/routers/users/loginRouter.js
@@ -6,6 +6,7 @@ export const loginRouter = Router()
 loginRouter.post('/Login', passport.authenticate('loginLocal', { failWithError: true }),
     async (req, res, next) => {
         res.session.user = JSON.stringify(req.user)
+        localStorage.setItem('user', JSON.stringify(req.user))
         res.status(204).json({ status: 'success', message: 'login success' })
     },
     (error, req, res, next) => {
