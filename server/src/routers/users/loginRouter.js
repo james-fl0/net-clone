b11aab6ae6e6b246afe31929eea63688f0533b68
Matias Gonzalez <matiasnahuelgonzalez01@gmail.com>
2024-01-14 18:17:48 -0300

    added jwt5

--- server/src/routers/users/loginRouter.js
+++ server/src/routers/users/loginRouter.js
@@ -8,7 +8,6 @@ loginRouter.post('/Login', async (req, res) => {
         const user = await userManager.login(req.body.email, req.body.password)
         const token =  jwt.sign(user, 'secret')
         req.session.token = token
-        console.log(procces.env.NODE_ENV);
         res.status(201).json({ status: 'success', message: 'login ok' })
     } catch (error) {
         res.status(401).json({ status: 'error', message: error.message })
