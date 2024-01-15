7e1be59259f2437d98acd7dd95c7c5aedd7f2242
Matias Gonzalez <matiasnahuelgonzalez01@gmail.com>
2024-01-15 18:21:54 -0300

    modify sessions

--- server/src/middlewares/sessions.js
+++ server/src/middlewares/sessions.js
@@ -14,7 +14,7 @@ export const sessions = session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
+    secure: process.env.NODE_ENV === 'development',
     httpOnly: true,
     domain: process.env.FRONTEND_URL_WHTTP || 'localhost',
     sameSite: 'None',
