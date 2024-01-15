475eb86dad85a7964e3e60c98fc803690158c085
Matias Gonzalez <matiasnahuelgonzalez01@gmail.com>
2024-01-15 19:16:59 -0300

    modify sessions11

--- server/src/routers/users/loginRouter.js
+++ server/src/routers/users/loginRouter.js
@@ -8,11 +8,11 @@ loginRouter.post('/Login', async (req, res) => {
         const user = await userManager.login(req.body.email, req.body.password)
         const token =  jwt.sign(user, 'secret')
          req.session.token = token
-         res.cookie('uth',token,{
+         res.cookie('uth','cacaaaaaa',{
             httpOnly: true,
             secure: false,
-            sameSite: 'Lax', 
-            domain: 'http://net-clone-xi.vercel.app', 
+            sameSite: 'None', 
+            domain: 'net-clone-xi.vercel.app', 
         })
         console.log(req.protocol)
         res.status(201).json({ status: 'success', message: 'login ok' })
