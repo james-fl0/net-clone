7a2e3e66385cec98834f870bf87a2c450199f5b0
James Harrison <james@fl0.com>
2024-01-16 15:26:09 -0800

    Updating cookie domain

--- server/src/routers/users/loginRouter.js
+++ server/src/routers/users/loginRouter.js
@@ -6,13 +6,13 @@ export const loginRouter = Router()
 loginRouter.post('/Login', async (req, res) => {
     try {
         const user = await userManager.login(req.body.email, req.body.password)
-        const token =  jwt.sign(user, 'secret')
-         req.session.token = token
-         res.cookie('uth','cacaaaaaa',{
+        const token = jwt.sign(user, 'secret')
+        req.session.token = token
+        res.cookie('uth', 'cacaaaaaa', {
             httpOnly: true,
             secure: true,
             sameSite: 'None',
-            domain:"net-clone-xi.vercel.app"
+            domain: process.env.FRONTEND_DOMAIN,
         })
         console.log(req.protocol)
         res.status(201).json({ status: 'success', message: 'login ok' })
