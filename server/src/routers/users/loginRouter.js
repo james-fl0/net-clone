1a6a1114dad1c468f2272ae519d2d2379524161b
Matias Gonzalez <matiasnahuelgonzalez01@gmail.com>
2024-01-16 18:44:18 -0300

    modify app 5

--- server/src/routers/users/loginRouter.js
+++ server/src/routers/users/loginRouter.js
@@ -12,7 +12,7 @@ loginRouter.post('/Login', async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: 'None', 
-            domain: 'net-clone-xi.vercel.app', 
+            domain: 'https://net-clone-xi.vercel.app', 
         })
         console.log(req.protocol)
         res.status(201).json({ status: 'success', message: 'login ok' })
